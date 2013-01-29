dec36643929156c3882c61d654b6eb88fcadee92
Brian Watterson <briansw@gmail.com>
2013-01-28 23:03:28 -0500

    remove 1b from index

--- index.php
+++ index.php
@@ -45,7 +45,6 @@
 	
 	<ul> <!- start accordian -->
 		
-		<?php include('studio-1b.php'); ?>
 		<?php include('lab-1a.php'); ?>
 		<?php include('readings.php'); ?>
 		<?php include('students.php'); ?>
