feaa054892f7c8210bf3d061fc6286b3b40053f4
Brian Watterson <briansw@gmail.com>
2013-01-31 22:42:46 -0500

    publish 1c

--- index.php
+++ index.php
@@ -45,6 +45,7 @@
 	
 	<ul> <!- start accordian -->
 		
+		<?php include('studio-1c.php'); ?>
 		<?php include('studio-1b.php'); ?>
 		<?php include('lab-1a.php'); ?>
 		<?php include('readings.php'); ?>
