60435d5d22eb806756cfd1d4f75c45441665d010
Brian Watterson <briansw@gmail.com>
2013-02-09 14:23:15 -0500

    fix metz pdf link

--- studio-2c.php
+++ studio-2c.php
@@ -167,7 +167,7 @@
 			<div class="column-3up"> <!- start column -->
 				<div class="file-wrapper">
 					<a class="no-underline" href="assets/files/Metz-Sandi-Practical-Object-Oriented-Design-In-Ruby.pdf"><img src="assets/images/icon-pdf.gif" width="37" height="49" /></a>
-					<p><a href="assets/files/Martens-Printed-Matter-What-Design-Means-for-Me.pdf"><em>Practical Object-Oriented Design in Ruby</em> (Ch. 1), Sandi Metz, 2012</a></p>
+					<p><a href="assets/files/Metz-Sandi-Practical-Object-Oriented-Design-In-Ruby.pdf"><em>Practical Object-Oriented Design in Ruby</em> (Ch. 1), Sandi Metz, 2012</a></p>
 					<p class="studio-due-date last">Due Week 3B</p>		
 				</div>
 			</div> <!- end column -->
