5009505043de862bc57e1ac1060e3bde0e853560
Brian Watterson <briansw@gmail.com>
2013-02-12 07:38:08 -0500

    week 3b

---
+++ studio-3b.php
@@ -0,0 +1,36 @@
+<li class="accordion-row">		
+	<div class="accordion-header">
+		<div class="icon studio-icon"></div>
+		<div class="title">Week 3B</div>
+		<span class="accordion-icon-closed">&ndash;</span>
+		<span class="accordion-icon-open">+</span>
+	</div>			
+	<div class="accordion-body">
+		<div class="discussion">
+			<h2>Discussion</h2>
+			<div class="slide-wrapper">
+				<div class="discussion-slide">
+					<img src="assets/images/studio/discussion-thumbs/martens-what-design-means-for-me.gif" width="75" />
+					<img src="assets/images/studio/discussion-thumbs/metz-ood.gif" width="75" />
+				</div>
+				<p class="last">Presentations by <a href="http://a.parsons.edu/~newtc116/">Caroline Newton</a> and <a href="http://a.parsons.edu/~merok742/">Kelly Merole</a></p>
+			</div>
+		</div>
+		<div class="lecture">
+			<h2>Small Group Critique</h2>
+			<div class="slide-wrapper">
+				
+				<p>&nbsp;</p>
+			</div>
+						
+		</div>
+					
+		<div class="assignments last">
+			<h2>Assignments</h2>			
+			<h3>Exercise 1</h3>
+			<p>Take the feedback received in class to refine and finalize your space website.</p>
+			<p class="studio-due-date">Due Week 3C</p>			
+		</div>
+		
+	</div>			
+</li> <!- end row -->
---
+++ studio-3c.php
@@ -0,0 +1,99 @@
+<li class="accordion-row">		
+	<div class="accordion-header">
+		<div class="icon studio-icon"></div>
+		<div class="title">Week 3C</div>
+		<span class="accordion-icon-closed">&ndash;</span>
+		<span class="accordion-icon-open">+</span>
+	</div>			
+	<div class="accordion-body">
+				<div class="lecture">
+			<h2>Lecture</h2>
+			<div class="slide-wrapper">
+				<div class="slide">
+					<iframe width="420" height="315" src="http://www.youtube.com/embed/0fKBhvDjuy0?rel=0" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<p><em>Powers of Ten</em>, Charles &amp; Ray Eames, 1977</p>
+			</div>
+			
+			<div class="slide-wrapper">
+				<div class="slide">
+					<iframe width="560" height="315" src="http://www.youtube.com/embed/EON7s2URjQw?rel=0" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<p><em>All-weather Garden Hose</em>, Brian Watterson, 2009</p>
+			</div>
+			
+			<div class="slide-wrapper">
+				<div class="slide">
+					<img src="assets/images/studio/project-1-lecture/klaas.jpg" alt="klaas" width="300" height="395" />
+				</div>
+				<p><em>Shivering Emotions and Feverish Feelings</em>, Klaas Kloosterboer, 2004</p>
+			</div>
+			
+			<div class="slide-wrapper">
+				<div class="slide">
+					<img src="assets/images/studio/project-1-lecture/ofoffjoff.jpg" alt="ofoffjoff" width="300" height="394" />
+				</div>
+				<p><em>One To One</em>, Ofoffjoff, 2007</p>
+			</div>
+			
+			<div class="slide-wrapper">
+				<div class="slide">
+					<img src="assets/images/studio/project-1-lecture/printed-matter.jpg" alt="printed-matter" width="300" height="403" />
+				</div>
+				<p><em>Printed Matter</em>, Karel Martens, 2000</p>
+			</div>
+			
+			<div class="slide-wrapper">
+				<div class="slide">
+					<img src="assets/images/studio/project-1-lecture/hamburg-pa.jpg" alt="hamburg-pa" width="603" height="517" />
+				</div>
+				<p><em>Hamburg, PA</em>, Brian Watterson, 2010</p>
+			</div>
+			
+			<div class="slide-wrapper">
+				<div class="slide">
+					<img src="assets/images/studio/project-1-lecture/bauhaus.png" alt="bauhaus" />
+				</div>
+				<p><em>Bauhaus: Weimar, Dessau, Berlin, Chicago</em>, Dr. Hedwig Wingler, 1978</p>
+			</div>
+			
+			<div class="slide-wrapper">
+				<div class="slide">
+					<img src="assets/images/studio/project-1-lecture/sealand.jpg" alt="sealand" width="400" height="271" />
+				</div>
+				<p><em>Sealand</em>, Metahaven</p>
+			</div>
+						
+		</div>
+					
+		<div class="assignments last">
+			<h2>Assignments</h2>			
+			<h3>Project 1</h3>
+			<p>Generate three separate narratives that you might like to pursue for your midterm and compile a list of references for each. Thoroughly define the key components within each narrative and sketch out in detail a proposed site structure and navigation. Finally, assemble a PDF presentation that presents each direction in a clear and coherent manner.</p>
+			<p class="studio-due-date">Due Week 4B</p>			
+		</div>
+		
+		<div class="readings">
+			<h2>Readings</h2>			
+			
+			<div class="column-3up"> <!- start column -->
+				<div class="file-wrapper">
+					<a class="no-underline" href="assets/files/van-der-Velden-Research-and-Destroy.pdf"><img src="assets/images/icon-pdf.gif" width="37" height="49" /></a>
+					<p><a href="assets/files/van-der-Velden-Research-and-Destroy.pdf"><em>Research &amp; Destroy</em>, Daniel van der Velden, 2006</a></p>
+					<p class="studio-due-date last">Due Week 4B</p>		
+				</div>
+			</div> <!- end column -->
+			
+			<div class="column-3up"> <!- start column -->
+				<div class="file-wrapper">
+					<a class="no-underline" href="assets/files/murakami-norwegian-wood-chapter-1.pdf"><img src="assets/images/icon-pdf.gif" width="37" height="49" /></a>
+					<p><a href="assets/files/murakami-norwegian-wood-chapter-1.pdf"><em>Norwegian Wood</em> (Ch. 1), Haruki Murakami, 1987</a></p>
+					<p class="studio-due-date last">Due Week 4B</p>		
+				</div>
+			</div> <!- end column -->
+
+
+		</div>
+		
+	</div>			
+</li> <!- end row -->
