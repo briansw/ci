3ddcd1f97b9409dca56ebdb4bba4ca3fedf7b856
Brian Watterson <briansw@gmail.com>
2013-03-17 16:24:41 -0400

    fix

--- lab-5a.php
+++ lab-5a.php
@@ -1,40 +1,30 @@
-<li class="accordion-row">		
-	<div class="accordion-header">
-		<div class="icon lab-icon"></div>
-		<div class="title">Week 5A</div>
-		<span class="accordion-icon-closed">&ndash;</span>
-		<span class="accordion-icon-open">+</span>
-	</div>			
-	<div class="accordion-body">
-		<div class="lecture">
-			<h2>Lecture</h2>
+<div class="lecture">
+	<h2>Lecture</h2>
 
-			<div class="slide-wrapper">
-				<div class="slide">
-					<p>Introduction to <em>cascading</em> style sheets</p>
-				</div>
-				<script src="https://gist.github.com/zackseuberling/5028035.js"></script>
-			</div>
-
-			<h2>Group critiques</h2>
-
-			<div class="slide-wrapper">
-				<div class="slide">
-					<p>More positioning!</p>
-				</div>
-				<script src="https://gist.github.com/zackseuberling/5027978.js"></script>
-			</div>
-
+	<div class="slide-wrapper">
+		<div class="slide">
+			<p>Introduction to <em>cascading</em> style sheets</p>
 		</div>
-		
-		<div class="assignments">
-			<h2>Assignments</h2>
-			
-			<h3>Exercise 3-b</h3>
-			<p>Execute your composition design using using HTML and CSS techniques discussed in class. Your site will not have any broken links, and it will not have any broken images. It will match your PDF designs as closely as possible.</p>
-			<p class="lab-due-date last">Due Week 6.</p>
+		<script src="https://gist.github.com/zackseuberling/5028035.js"></script>
+	</div>
+
+	<h2>Group critiques</h2>
+
+	<div class="slide-wrapper">
+		<div class="slide">
+			<p>More positioning!</p>
 		</div>
-									
-	</div> <!-- end body -->
+		<script src="https://gist.github.com/zackseuberling/5027978.js"></script>
+	</div>
+
+</div>
+
+<div class="assignments">
+	<h2>Assignments</h2>
 	
-</li> <!-- end row -->
+	<h3>Exercise 3-b</h3>
+	<p>Execute your composition design using using HTML and CSS techniques discussed in class. Your site will not have any broken links, and it will not have any broken images. It will match your PDF designs as closely as possible.</p>
+	<p class="lab-due-date last">Due Week 6.</p>
+</div>
+							
+</div> <!-- end body -->
