18617d807c3d951f4757cc2833d30327803c6644
Brendan Griffiths <brendangriffiths@gmail.com>
2013-08-28 15:22:22 -0400

    adding lab week 1

---
+++ lab-1b.php
@@ -0,0 +1,116 @@
+		<div class="lecture">
+			<h2>Lecture</h2>
+
+			<div class="slide-wrapper">
+				<div class="slide">
+					<img src="assets/images/lab/server-setup-file-management.png" alt="server-setup-file-management" width="618" height="120" />
+				</div>
+				<p>Our standard FTP set up</p>
+			</div>
+
+			<div class="slide-wrapper">
+				<div class="slide">
+					<img src="assets/images/lab/server-setup-file-management-2.png" alt="server-setup-file-management" width="618" height="120" />
+				</div>
+				<p>Setting up our first project</p>
+			</div>
+
+
+
+			<h3>Connecting to the server</h3>
+				<p>To connect to your Parsons web space, use Fetch (or your preferred SFTP client) with the following settings:</p>
+
+			<div class="slide-wrapper">
+				<div class="slide">
+					<img src="assets/images/lab/fetch_config.jpg" alt="fetch-configuration" />
+				</div>
+				<p>Be sure to select <strong>SFTP</strong> from the <em>Connect using</em> drop down menu.</p>
+			</div>
+
+			<p>You can find your Username and Password for your A.PARSONS.EDU account in the "User Information" letter sent to your NewSchool email address. It should look something like this:</p>
+
+			<div class="slide-wrapper">
+				<div class="slide">
+					<img src="assets/images/lab/welcome.jpg" alt="welcome-email" />
+				</div>
+			</div>
+
+			<h3>Basic HTML page and tags</h3>
+			<p>The basic HTML page structure is as follows:</p>
+			
+			<script src="https://gist.github.com/1657949.js"></script>
+
+			<p>Below are some common HTML tags that you can use to begin building your composition and index page. <a href="samples/basictags.html" target="_blank">This page shows how they render in your browser.</a></p>
+			
+
+			
+
+
+			<div class="slide-wrapper">
+				<div class="slide">
+					<img src="assets/images/lab/composition-der-dada.jpg" alt="composition-der-dada" width="300" height="410" />
+				</div>
+				<p>Cover of <em>der Dada vol1</em>, 1919</p>
+			</div>
+
+
+			<div class="slide-wrapper">
+				<div class="slide">
+					<img src="assets/images/lab/composition-merz.jpg" alt="composition-merz" width="300" height="396" />
+				</div>
+				<p>Kurt Schwitters, 1924</p>
+			</div>
+			
+
+			<div class="slide-wrapper">
+				<div class="slide">
+					<img src="assets/images/lab/williams-abcdetc.jpg" alt="williams-abcdetc" />
+				</div>
+				<p><em>abcdefghijklmnopqrstuvwxyz</em>, Emmett Williams, 1963</p>
+			</div>
+
+
+			<div class="slide-wrapper">
+				<div class="slide">
+					<img src="assets/images/lab/composition-Muller-Brockmann.jpeg" alt="composition-Muller-Brockmann" width="298" height="420" />
+				</div>
+				<p>Josef Müller-Brockmann, 1972</p>
+			</div>
+			
+			<div class="slide-wrapper">
+				<div class="slide">
+					<img src="assets/images/lab/composition-the-389.png" alt="composition-the-389" width="1057" height="606" />
+				</div>
+				<p><a href="http://the389.com/">The 389</a>, 2011</p>
+			</div>
+
+			<script src="https://gist.github.com/1658418.js"></script>
+
+
+		</div> <!-- end lecture -->
+		
+		<div class="assignments">
+			<h2>Assignments</h2>
+			
+			<h3>Exercise 1</h3>
+			<p>Using the elements listed above, create a formal composition that you find beautiful. How can you utilize elements about the web to emphasis emotions or convey ideas? What ideas and interactions are unique to the web? In what ways can use the language of the web to convey new ideas?</p>
+			<p class="lab-due-date first">Due Week 3B</p>	
+
+			<h3>Assignment 1</h3>
+			<p>Build an index page for yourself using the structure and tags used in today’s lecture. Consider how the page could reflect your particular design sensibility. We will be using this page as a springboard to link to the projects you make this semester, so think about how the structure could evolve to accommodate a living archive of work.</p>
+			<p>Remember to name your file <code>index.html</code> before uploading. This will allow it to be viewed at the root level of your New School webspace, for example: <code>http://a.parsons.edu/~your_net_id/</code>.</p>
+			<p class="lab-due-date last">Ongoing, First Iteration Due Week 3B</p>	
+		</div>
+							
+		<div class="readings">
+			<h2>Readings</h2>			
+			
+			<div class="column-3up"> <!- start column -->
+				<div class="file-wrapper">
+					<a class="no-underline" href="assets/files/lethem_ecstasy_of_influence.pdf"><img src="assets/images/icon-pdf.gif" width="37" height="49" /></a>
+					<p><a href="assets/files/lethem_ecstasy_of_influence.pdf"><em>The Ecstasy of Influence</em>, Jonathan Lethem, 2007</a></p>
+					<p class="lab-due-date last">Due Week 2A</p>		
+				</div>
+			</div> <!- end column -->
+
+		</div> <!-- end readings -->
