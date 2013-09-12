365dbda94c5455f0da2af2d630f70103effed930
Brian Watterson <briansw@gmail.com>
2013-09-12 08:44:59 -0400

    week 3c

--- index.php
+++ index.php
@@ -48,6 +48,7 @@
 
     <ul> <!- start accordian -->
 
+        <?php include('snippets/studio-3c.php'); ?>
         <?php include('snippets/lab-3b.php'); ?>
         <?php include('snippets/studio-2a.php'); ?>
         <?php include('snippets/studio-1c.php'); ?>
