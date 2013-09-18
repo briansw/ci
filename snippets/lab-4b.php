bd8820e07be6061d5803e81db51df9a70a389ab0
Brendan Griffiths <brendangriffiths@gmail.com>
2013-09-18 18:45:35 -0400

    adding wk 4 lab

--- snippets/lab-4b.php
+++ snippets/lab-4b.php
@@ -1,7 +1,7 @@
-<li class="accordion-row" data-url="lab-2b.php">
+<li class="accordion-row" data-url="lab-4b.php">
 	<div class="accordion-header">
 		<div class="icon lab-icon"></div>
-		<div class="title">Week 2B</div>
+		<div class="title">Week 4B</div>
 		<span class="accordion-icon-closed">&ndash;</span>
 		<span class="accordion-icon-open">+</span>
 	</div>			
