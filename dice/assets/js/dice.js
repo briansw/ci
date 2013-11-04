f36daed9ca3b06316ee4bda69a00ef73c595392e
Brian Watterson <briansw@gmail.com>
2013-11-03 19:12:19 -0500

    dice game v1

--- dice/assets/js/dice.js
+++ dice/assets/js/dice.js
@@ -2,7 +2,7 @@ var Game = {
     current_player: 0,
     next_player: 0,
     players: 2,
-    rolls: 1,
+    rolls: 3,
     turns: 1,
     dice: 2,
     dice_sides: 6,
@@ -24,7 +24,8 @@ function begin_roll() {
     set_current_player(); // Don't touch this!
     set_roll_score(roll_dice());
     set_turn_score();
-    check_for_winner();
+    check_for_winner('highest');
+    render_roll();
 }
 
 function initialize_game() {
@@ -34,6 +35,15 @@ function initialize_game() {
             roll_scores: [],
             total_score: 0
         };
+        $(".players").append("<div class='player-" + i +"'>");
+        $(".players").append("<h2>Player: <span id='player-" + i +"'>" + i + "</span></h2>");
+        $(".players").append("<h4>Current Roll: <span id='player-" + i +"-current'></span>");
+        $(".players").append("<h4>Roll Score: <span id='player-" + i +"-roll-score'></span>");
+        $(".players").append("<h4>Turn Score: <span id='player-" + i +"-turn-score'></span>");
+        $(".players").append("<h4>Total Score: <span id='player-" + i +"-total-score'></span>");
+        $(".players").append("</div>");
+
+
     }
 }
 
@@ -85,6 +95,7 @@ function set_roll_score(dice) {
     console.log(Game.current_player);
     players[Game.current_player].roll_scores.push(roll_score);
     console.log(players[Game.current_player].roll_scores);
+
 }
 
 function set_turn_score() {
@@ -105,20 +116,45 @@ function set_turn_score() {
     }
 }
 
-function check_for_winner() {
+function check_for_winner(condition) {
     if ((Game.turns != 0) && (Game.total_turns > Game.turns)) {
-        var highest_score = 0;
-        $(players).each(function (x) {
-            if (players[x].total_score > highest_score) {
-                highest_score = players[x].total_score;
-                Game.winning_player = x;
-            }
-        });
-        alert(Game.winning_player + ' wins with a score of ' + players[Game.winning_player].total_score + '!');
-    } else {
-        if (players[Game.current_player].total_score >= Game.winning_score) {
-            Game.winning_player = Game.current_player
-            alert(Game.winning_player + ' wins with a score of ' + players[Game.current_player].total_score + '!');
+        if (condition == 'highest') {
+            find_highest_score();
+        } else if (condition == 'lowest') {
+            find_lowest_score();
+        } else {
+            find_closest_score(condition);
         }
+    } else if ((Game.turns == 0) && (Game.total_turns >= Game.players)) {
+
     }
+}
+
+//function find_highest_score() {
+//    if ((Game.turns != 0) && (Game.total_turns > Game.turns)) {
+//        //        var highest_score = 0;
+//        $(players).each(function (x) {
+//            if (players[x].total_score >= highest_score) {
+//                if (players[x].total_score == highest_score) {
+//                    //draw
+//                }
+//                highest_score = players[x].total_score;
+//                Game.winning_player = x;
+//            }
+//        });
+//        alert(Game.winning_player + ' wins with a score of ' + players[Game.winning_player].total_score + '!');
+//
+//    } else {
+//        if ((players[Game.current_player].total_score >= Game.winning_score) && (Game.total_turns > Game.turns)) {
+//            Game.winning_player = Game.current_player
+//            alert(Game.winning_player + ' wins with a score of ' + players[Game.current_player].total_score + '!');
+//        }
+//    }
+//}
+
+
+function render_roll() {
+    $("#player-" + Game.current_player + "-current").html(players[Game.current_player].current_roll);
+    $("#player-" + Game.current_player + "-roll-score").html(players[Game.current_player].roll_scores + ',');
+    $("#player-" + Game.current_player + "-turn-score").html(players[Game.current_player].total_score);
 }
