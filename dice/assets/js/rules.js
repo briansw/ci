de121cdfb2f6a41a24be77d3c65c152c52ff1da8
Brendan Griffiths <brendangriffiths@gmail.com>
2013-11-18 23:31:59 -0500

    some notes

--- dice/assets/js/rules.js
+++ dice/assets/js/rules.js
@@ -22,10 +22,11 @@ var Game = {
     // Value of winning score
     winning_score: 5,
 
-    // Determines the winning condition in relation to the winning score value
+    // Determines the winning condition in relation to the winning score value and number of turns
     // Allowed values: 'highest', 'lowest', or 'matching'
-    // Example: Highest score that is ....
-    // Example: Lowest ....
+    // Example: If winning_score is set to 5, turns is set to 0, and you set win_condition to 'highest', the player with the highest score above 5 at the end of any turn will be declared a winner.
+    // Example: If turns is set to a finite number (3, for example), and you set win_condition to 'highest', the player with the highest score at the end of three rounds will be declared a winner, irrespective of the value of winning_score
+    // Example: If winning_score is set to 5 and turns is set to 0, and you set win_condition to 'matching', the game will continue until a player has a score which is exactly 5. In this case, be sure your game logic allows player scores to rise and fall with each round.
     win_condition: 'highest',
 
     // Initial score each player starts at
