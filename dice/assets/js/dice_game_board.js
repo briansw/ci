87c0854ba8a458c3211bd22ef9d52711113eb761
Brendan Griffiths <brendangriffiths@gmail.com>
2013-11-19 00:02:09 -0500

    game board updates

--- dice/assets/js/dice_game_board.js
+++ dice/assets/js/dice_game_board.js
@@ -11,13 +11,12 @@ $('.roll').click(function() {
 // Draws the game board on page load
 function initialize_game_board() {
     for (var i = 0; i < Game.players; i++) {
-        $("#game-board").append("<div class='player-" + i +"'>");
-        $("#game-board").append("<h2>Player: <span id='player-" + i +"'>" + i + "</span></h2>");
-        $("#game-board").append("<h4>Current Roll: <span id='player-" + i +"-current'></span>");
-        $("#game-board").append("<h4>Roll Score: <span id='player-" + i +"-roll-score'></span>");
-        $("#game-board").append("<h4>Turn Score: <span id='player-" + i +"-turn-score'></span>");
-        $("#game-board").append("<h4>Total Score: <span id='player-" + i +"-total-score'></span>");
-        $("#game-board").append("</div>");
+        $("#game-board").append("<div class='player-" + i +"'></div>");
+        $(".player-" + i).append("<h2>Player: <span id='player-" + i +"'>" + i + "</span></h2>");
+        $(".player-" + i).append("<h4>Current Roll: <span id='player-" + i +"-current'>" + players[i].current_roll + "</span></h4>");
+        $(".player-" + i).append("<h4>Roll Score: <span id='player-" + i +"-roll-score'>" + players[i].roll_scores + "</span></h4>");
+        $(".player-" + i).append("<h4>Turn Score: <span id='player-" + i +"-turn-score'></span></h4>");
+        $(".player-" + i).append("<h4>Total Score: <span id='player-" + i +"-total-score'>" + players[i].total_score + "</span></h4>");
     }
 }
 
@@ -25,9 +24,10 @@ function initialize_game_board() {
 // Redraws the current player's portion of the game
 // board at the end of their turn
 function render_roll() {
+
     $("#player-" + Game.current_player + "-current").html(players[Game.current_player].current_roll);
     $("#player-" + Game.current_player + "-roll-score").html(players[Game.current_player].roll_scores + ',');
-    $("#player-" + Game.current_player + "-turn-score").html(players[Game.current_player].total_score);
+    $("#player-" + Game.current_player + "-total-score").html(players[Game.current_player].total_score);
 }
 
 
