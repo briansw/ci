015d08e631ce25ff1994d27cc667d6f04d0768dd
Brian Watterson <briansw@gmail.com>
2014-01-23 17:06:04 -0500

    remove student favicons

--- favicon.php
+++ favicon.php
@@ -1,6 +1,6 @@
 <?php
 
-$favicons = array('favicon.png', '1.ico', '2.png', '3.png', '4.jpg', '5.jpg', '6.ico', '7.png', '8.gif', '9.jpg');
+$favicons = array('favicon.png');
 $key = array_rand($favicons);
 $favicon_to_use = $favicons[$key];
 
