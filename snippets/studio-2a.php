6b3350a39839a0f2d0d1bb60c1a5e609b58d23f7
Brian Watterson <briansw@gmail.com>
2014-02-04 07:31:53 -0500

    publish 2b

--- snippets/studio-2a.php
+++ snippets/studio-2a.php
@@ -1,7 +1,7 @@
 <li class="accordion-row" data-url="studio-2a.php">
 	<div class="accordion-header">
 		<div class="icon studio-icon"></div>
-		<div class="title">Week 2A</div>
+		<div class="title">Week 2B</div>
 		<span class="accordion-icon-closed">&ndash;</span>
 		<span class="accordion-icon-open">+</span>
 	</div>			
