355de8e2f236350bc84afa2482858372610974bc
Brian Watterson <briansw@gmail.com>
2014-03-13 07:42:39 -0400

    update for week 7c

--- index.php
+++ index.php
@@ -53,9 +53,9 @@
     <?#php include('snippets/studio-10a.php'); ?>
     <?#php include('snippets/lab-9b.php'); ?>
     <?#php include('snippets/studio-9a.php'); ?>
-    <?#php include('snippets/studio-8c.php'); ?>
     <?#php include('snippets/lab-4b.php'); ?>
 
+        <?php include('snippets/studio-8c.php'); ?>
         <?php include('snippets/studio-8a.php'); ?>
         <?php include('snippets/studio-6c.php'); ?>
         <?php include('snippets/studio-4a.php'); ?>
