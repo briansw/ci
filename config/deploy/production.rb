d4561737fdab564339f141e7cd609f5aca0d43dd
Brian Watterson <briansw@gmail.com>
2014-06-16 19:58:56 -0400

    capify

---
+++ config/deploy/production.rb
@@ -0,0 +1,20 @@
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary
+# server in each group is considered to be the first
+# unless any hosts have the primary property set.
+# Don't declare `role :all`, it's a meta role
+role :app, %w{briansw@briansw.net}
+role :web, %w{briansw@briansw.net}
+role :db,  %w{briansw@briansw.net}
+
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server
+# definition into the server list. The second argument
+# something that quacks like a hash can be used to set
+# extended properties on the server.
+server 'briansw.net', user: 'briansw', roles: %w{web app}
+
+set :deploy_to, '/home/briansw/ci.brianwatterson.com'
+set :branch, :master
