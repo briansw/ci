e6a32b4b5e9eb9d03504f6062b7c425f42777ae6
Brian Watterson <briansw@gmail.com>
2014-06-16 21:28:17 -0400

    change url

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{briansw@briansw.net}
-role :web, %w{briansw@briansw.net}
-role :db,  %w{briansw@briansw.net}
+role :app, %w{briansw@brianwatterson.com}
+role :web, %w{briansw@brianwatterson.com}
+role :db,  %w{briansw@brianwatterson.com}
 
 # Extended Server Syntax
 # ======================
@@ -14,7 +14,7 @@ role :db,  %w{briansw@briansw.net}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'briansw.net', user: 'briansw', roles: %w{web app}
+server 'brianwatterson.com', user: 'briansw', roles: %w{web app}
 
 set :deploy_to, '/home/briansw/ci.bw.com'
 set :branch, :master
