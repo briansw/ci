ca040b3db985a1b8cc48a8a379daeeec18251b00
Brian Watterson <briansw@gmail.com>
2014-08-26 17:52:31 -0400

    begin work on presentation mode

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,5 @@
 //= require jquery_ujs
 //= require filters
 //= require students
+//= require slick
+//= require presentation_mode
---
+++ app/assets/javascripts/presentation_mode.js
@@ -0,0 +1,43 @@
+$(document).keydown(function(e) {
+  var $presentation_overlay = $('#overlay.presentation-wrapper');
+
+  if (e.which == 80) {
+    var content_blocks = [];
+    $('.content-block').each(function() {
+      content_blocks.push($(this).clone());
+    });
+
+    $presentation_overlay.children('.slides').first().html(content_blocks);
+
+    if ($presentation_overlay.children().length > 0) {
+      $presentation_overlay.addClass('visible');
+      $('#overlay.presentation-wrapper .slides').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        arrows: true,
+        dots: false,
+        infinite: true,
+        speed: 1000,
+        pauseOnHover: false,
+        swipe: true
+      });
+    }
+  }
+
+});
+
+//$('.slideshow-link').click(function(event) {
+//  event.preventDefault();
+//  var slide_to_show = $(this).data('slide-index');
+//  $('.slick-initialized').slickGoTo(slide_to_show);
+//  $('.slideshow-block').toggleClass('visible');
+//  $('.close-button').show();
+//});
+//
+//$('.close-button').click(function(event) {
+//  event.preventDefault();
+//  $('.slideshow-block').removeClass('visible');
+//  $(this).hide();
+//});
