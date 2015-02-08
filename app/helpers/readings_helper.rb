51e005433483f476add071b8faa17651689924a8
Brian Watterson <briansw@gmail.com>
2015-02-08 15:57:31 -0500

    check for link otherwise link to pdf

--- app/helpers/readings_helper.rb
+++ app/helpers/readings_helper.rb
@@ -1,10 +1,10 @@
 module ReadingsHelper
 
   def pdf_or_url(reading)
-    if reading.pdf.present?
-      reading.pdf.attachment_url
-    else
+    if reading.link.present?
       reading.link
+    else
+      reading.pdf.attachment_url
     end
   end
 
