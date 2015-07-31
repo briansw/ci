15d690a4501ca8410add9328b9a21692b53e13ca
Brian Watterson <briansw@gmail.com>
2015-07-31 16:01:15 -0400

    better filter ui

--- app/assets/javascripts/filters.js
+++ app/assets/javascripts/filters.js
@@ -1,19 +1,19 @@
 $('.bug').click(function() {
-  var target   = $(this).data('target');
-  if ($(this).hasClass('off')) {
-    $('.studio-section, .lab-section').fadeTo('fast','1').find('a').removeClass('disabled');
-    $(this).removeClass('off');
+  var target = $(this).data('target');
+
+  if (target == 'all') {
+    $('.lab-section').fadeTo('fast','1').find('a').removeClass('disabled');
+    $('.studio-section').fadeTo('fast','1').find('a').removeClass('disabled');
+  } else if (target == 'studio') {
+    $('.lab-section').fadeTo('fast','0').find('a').addClass('disabled');
+    $('.studio-section').fadeTo('fast','1').find('a').removeClass('disabled');
   } else {
-    if (target == 'studio') {
-      $('.lab-section').fadeTo('fast','0').find('a').addClass('disabled');
-      $('.studio-section').fadeTo('fast','1').find('a').removeClass('disabled');
-    } else {
-      $('.studio-section').fadeTo('fast','0').find('a').addClass('disabled');
-      $('.lab-section').fadeTo('fast','1').find('a').removeClass('disabled');
-    }
-    $('#filters .bug').not(this).addClass('off');
-    $(this).removeClass('off');
+    $('.lab-section').fadeTo('fast','1').find('a').removeClass('disabled');
+    $('.studio-section').fadeTo('fast','0').find('a').addClass('disabled');
   }
+
+  $('#filters .bug').not(this).addClass('off');
+  $(this).removeClass('off');
 });
 
 $("body").on( 'click', '.disabled', function(e) {
